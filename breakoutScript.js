e906164ac60101cfd2e4aa03e884c8cf2b1d3286
K4 <llamandoakalos@gmail.com>
2025-11-11 21:57:39 -0700

    feat: Adds paddle and its movement

--- breakoutScript.js
+++ breakoutScript.js
@@ -1,15 +1,31 @@
 const canvas = document.getElementById('breakoutCanvas');
 const ctx = canvas.getContext('2d');
 
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
 
+
+// En esta área se definen los objetos
 let ball = {
     x: canvas.width / 2,
     y: canvas.height -30,
     radius: 10,
-    dx: 2,
-    dy: -2
+    dx: 4,
+    dy: -4
 }
 
+let paddle = {
+    h: 10,
+    w: 75,
+    position: 0,
+    rightPressed: false,
+    leftPressed: false
+}
+
+//Solucion a paradoja temporal
+paddle.position = (canvas.width - paddle.w) /2;
+
+// En esta área se dibujan los objetos
 function drawBall() {
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI *2);
@@ -18,7 +34,16 @@ function drawBall() {
     ctx.closePath()
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.position, canvas.height-paddle.h, paddle.w, paddle.h);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+}
+
+//Aqui actualizamos los valores de los objetos
 function updateFrames() {
+    //Colision con Paredes
     if (ball.x + ball.dx > canvas.width || ball.x + ball.dx < 0) {
         ball.dx = -ball.dx;
     }
@@ -27,13 +52,42 @@ function updateFrames() {
         ball.dy = -ball.dy;
     }
 
+    //Movimiento del Paddle
+    //Incluye colision con las paredes
+    if (paddle.rightPressed && paddle.position < canvas.width - paddle.w){
+        paddle.position += 7;
+    } else if (paddle.leftPressed && paddle.position > 0){
+        paddle.position -= 7;
+    }
+
+    // Movimiento de la Pelota
     ball.x += ball.dx;
     ball.y += ball.dy;
 }
 
+// Aqui se encuantran las funciones de controles
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        paddle.rightPressed = true;
+    } else if (e.keyCode == 37) {
+        paddle.leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        paddle.rightPressed = false;
+    } else if (e.keyCode == 37) {
+        paddle.leftPressed = false;
+    }
+}
+
+// Aquí se reproduce toa la sentencia del juego
 function gameLoop() {
+    //Limpia el canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     updateFrames();
+    drawPaddle();
     drawBall();
     requestAnimationFrame(gameLoop);
 }
